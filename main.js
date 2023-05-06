58ad560eae5674a9c53afe1ebea7b4f9fa9ec17e
Abhix <abhinabasen2022@gmail.com>
2023-05-06 15:07:56 +0530

    rrer

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 status1 = "";
-valuei = "";
+valueI = "";
+objects = [];
+
+
+const synth = window.speechSynthesis;
 
 function setup(){
     canvas = createCanvas(400,400);
@@ -10,6 +14,33 @@ function setup(){
 
 function draw(){
     image(video,0,0,400,400);
+
+    if (status1 != ""){
+        objectDetector.detect(video, gotResult);
+
+        for(i = 0; i < objects.length; i++){
+            fill('red')
+            text(objects[i].label + " " +  Math.floor(objects[i].confidence * 100)  + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke('yellow');
+            rect(objects[i].x , objects[i].y ,objects[i].width ,objects[i].height);
+
+
+            if (objects[i].label == valueI){
+
+                const utter = new SpeechSynthesisUtterance(valueI + " " + "has been found!");
+
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("ifde").innerHTML =  valueI + " " + "Found!";
+                synth.speak(utter);
+            }
+            else {
+                console.log(valuei)
+                document.getElementById("ifde").innerHTML =  valueI + " " + " Not Found!";
+            }
+        }
+    }
 }
 
 function start(){
@@ -21,4 +52,16 @@ function start(){
 function modelLoaded(){
     console.log("model loaded!");
     status1 = true;
+}
+
+
+function gotResult(error,results){
+
+    if (error){
+        console.error(error)
+    }
+    else {
+        objects = results;
+    }
+
 }
